4d6d160a93a97074841c902bd47523eee50a833b
daoyi7 <kawhichina@gmail.com>
2018-01-19 21:37:13 +0800

    Frida init

---
+++ src/main.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { AppContainer } from 'react-hot-loader'
+import App from './App'
+
+const render = (App) => {
+  ReactDOM.render(
+    <AppContainer>
+      <App />
+    </AppContainer>,
+    document.getElementById('app')
+  )
+}
+
+render(App)
+
+if (module.hot) {
+  module.hot.accept('./App', () => render(App))
+}
+
+// ReactDom.render(
+//   <h1>Hello, Frida!</h1>,
+//   document.getElementById('app')
+// )
