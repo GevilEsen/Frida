dd6d3a3990916bdebd1a455e407f328522f31707
daoyi7 <codingwell@gmail.com>
2018-02-13 23:13:53 +0800

    update

--- src/containers/home/index.jsx
+++ src/containers/home/index.jsx
@@ -11,7 +11,7 @@ class Home extends React.Component {
     return (
       <div>
         <h2>Hey, Frida!</h2>
-        <Button type="primary">Primary</Button>
+        <Button type="primary">按钮</Button>
       </div>
     )
   }
