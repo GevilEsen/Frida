b3ab931b5d752560e572fe9b70a459fd2237d4f7
daoyi7 <codingwell@gmail.com>
2018-02-13 23:12:05 +0800

    update

--- entry/index.js
+++ entry/index.js
@@ -2,4 +2,5 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import Home from '../src/containers/home/index.jsx'
 
-ReactDOM.render(<Home/>, document.getElementById('app'))
+ReactDOM.render(
+  <Home/>, document.getElementById('app'))
--- src/containers/home/index.jsx
+++ src/containers/home/index.jsx
@@ -1,20 +1,20 @@
 import React from 'react'
 import classNames from 'classnames'
-import { Button } from 'antd'
+import {Button} from 'antd'
 
 class Home extends React.Component {
-    constructor(props) {
-        super(props)
-    }
+  constructor(props) {
+    super(props)
+  }
 
-    render() {
-        return (
-            <div>
-                <h2>Hey, Frida!</h2>
-                <Button type="primary">Primary</Button>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <h2>Hey, Frida!</h2>
+        <Button type="primary">Primary</Button>
+      </div>
+    )
+  }
 }
 
 export default Home
